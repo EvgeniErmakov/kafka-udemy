65300aef90ef1aee9c614af7efc230d9337ccb3c
eermakov <EVErmakov@lanit.ru>
2023-02-08 22:14:17 +0300

    add a simple kafka publisher example

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class Main {
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
+        properties.setProperty("key.serializer", StringSerializer.class.getName());
+        properties.setProperty("value.serializer", StringSerializer.class.getName());
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        ProducerRecord<String, String> producerRecord = new ProducerRecord<>("first_topic", "hello kafka");
+
+        producer.send(producerRecord);
+
+        producer.flush();
+
+        producer.close();
+    }
+}
