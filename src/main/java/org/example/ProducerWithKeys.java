94c6ca0776841dd228c05e86cf416809818ddb75
eermakov <EVErmakov@lanit.ru>
2023-02-09 20:42:29 +0300

    add producer with keys

---
+++ src/main/java/org/example/ProducerWithKeys.java
@@ -0,0 +1,44 @@
+package org.example;
+
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerWithKeys {
+    private static final Logger log = LoggerFactory.getLogger(ProducerCallbacks.class);
+
+    public static void main(String[] args) throws InterruptedException {
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
+        properties.setProperty("key.serializer", StringSerializer.class.getName());
+        properties.setProperty("value.serializer", StringSerializer.class.getName());
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        for (int x = 0; x < 2; x++) {
+            for (int i = 0; i < 10; i++) {
+                String topic = "second_topic";
+                String key = "id_" + i;
+                String value = "hello world " + i;
+
+                ProducerRecord<String, String> producerRecord = new ProducerRecord<>(topic, key, value);
+
+                producer.send(producerRecord, new Callback() {
+                    @Override
+                    public void onCompletion(RecordMetadata metadata, Exception exception) {
+                        log.info("Topic: " + metadata.topic() + " Key: " + key + " Partition: " + metadata.partition());
+                    }
+                });
+            }
+        }
+
+        producer.flush();
+        producer.close();
+    }
+}
